63a77c21e6b79be618dc8f982b4aef5021642475
filipjt88 <filip88ks@gmail.com>
2025-04-28 23:40:45 +0200

    save db

---
+++ quiz.sql
@@ -0,0 +1,9 @@
+CREATE TABLE questions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    question TEXT NOT NULL,
+    option_a VARCHAR(255) NOT NULL,
+    option_b VARCHAR(255) NOT NULL,
+    option_c VARCHAR(255) NOT NULL,
+    option_d VARCHAR(255) NOT NULL,
+    correct_option CHAR(1) NOT NULL
+);
