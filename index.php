7724c6c11bc9354609a099364ef41c61700e7579
filipjt88 <filip88ks@gmail.com>
2025-04-28 23:12:28 +0200

    save

--- index.php
+++ index.php
@@ -1,22 +1,12 @@
 <?php $title = "Quiz"; ?>
 
 <?php include 'parts/top.php'; ?>
-
 <div class="container bg-light py-5">
     <h1 class="text-center mb-4">Quiz</h1>
-    
     <div id="quiz-container" class="card shadow p-4">
         <!-- Ovde će JavaScript ubacivati pitanja -->
     </div>
-
     <div id="result" class="text-center mt-4"></div>
 </div>
-
-
-
-
-
-
-
 <?php include 'parts/bottom.php'; ?>
     
